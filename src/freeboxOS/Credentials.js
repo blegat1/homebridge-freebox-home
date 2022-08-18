463d39a8f6aaf7c61b749786a28ae8da118eb8fa
blegat1 <75172707+blegat1@users.noreply.github.com>
2022-08-18 11:49:04 +0200

    check credentials file size

--- src/freeboxOS/Credentials.js
+++ src/freeboxOS/Credentials.js
@@ -1,14 +1,13 @@
 const fs = require('fs') 
-//const envfile = require('envfile')
 
 let credFilePath = './src/.store'
 
 module.exports.update = function(token, track, callback) {
 	if(fs.existsSync(credFilePath)) {
 		let data = {
-            token: token,
-            track: track
-        }
+            		token: token,
+            		track: track
+        	}
 		fs.writeFile(credFilePath, JSON.stringify(data), (err) => {
 			console.log("[-] Updated Store file")
 			callback(true)
@@ -17,29 +16,31 @@ module.exports.update = function(token, track, callback) {
 }
 
 module.exports.checkCredentialsFile = function() {
-	if(!fs.existsSync(credFilePath)) {
-        let data = {
-            token: null,
-            track: null
-        }
-		fs.writeFile(credFilePath, JSON.stringify(data), (err) => {
-			console.log("[-] Created Store file")
-		})
-		return true
-	} else {
-		console.log('[-] Store file already present')
-		return false
-	}
+	fs.stat(credFilePath, (err, stats) => {
+		if (err || !stats.isFile() || stats.size == 0) {
+			let data = {
+			    token: null,
+			    track: null
+			}
+			fs.writeFile(credFilePath, JSON.stringify(data), (err) => {
+				console.log("[-] Created Store file")
+			})
+			return true
+		} else {
+			console.log('[-] Store file already present')
+			return false
+		}   
+	})
 }
 
 module.exports.getStoredCredentials = function(callback) {
 	if(fs.existsSync(credFilePath)) {
-        fs.readFile(credFilePath, (err, data) => {
-            let credentials = JSON.parse(data)
-            callback({
-                token: credentials.token,
-                track: credentials.track
-            })
-        })
-    }
+		fs.readFile(credFilePath, (err, data) => {
+		    let credentials = JSON.parse(data)
+		    callback({
+			token: credentials.token,
+			track: credentials.track
+		    })
+		})
+    	}
 }
